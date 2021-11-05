e3ed4ddce70c388590c7984740ea6d2cdfe8a772
Emmanuel Izilov <em5750@gmail.com>
2021-11-05 14:54:29 -0400

    scroll to top implemented

---
+++ index.js
@@ -0,0 +1,9 @@
+const btnScrollToTop = document.getElementById('btnScrollToTop')
+
+btnScrollToTop.addEventListener('click', function(){
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+})
